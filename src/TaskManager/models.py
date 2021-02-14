6ab6a20eb4ad47b81eaa6531a74dddccdc2e86ee
k-ken-souce <chirag21works@gmail.com>
2021-02-14 09:58:44 +0530

    TaksManager

--- src/TaskManager/models.py
+++ src/TaskManager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from users.models import Profile
+from django.shortcuts import render, redirect, reverse
+from django.utils import timezone
 
+class Task(models.Model):
+	profile  = 
 
